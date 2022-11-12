5ae4f76b924b452551b6976da64d4d3a2a4f917c
Danrodlag <drodenaslagares@gmail.com>
2022-11-12 18:47:34 +0100

    Falta comentar

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ def dado(i):
     resuldado = randint(1, 6)
     posibilidades = {
         1: "\n\n\t*\n\n",
-        2: "\n\t\t*\n\n*\n",
-        3: "\n\t\t*\n\t*\n*\n",
-        4: "\n*\t\t*\n\n*\t\t*\n",
-        5: "\n*\t\t*\n\t*\n*\t\t*\n",
-        6: "\n*\t\t*\n*\t\t*\n*\t\t*\n"
+        2: "\n\t\t*\n\n\n\n*\n",
+        3: "\n\t\t*\n\n\t*\n\n*\n",
+        4: "\n*\t\t*\n\n\n\n*\t\t*\n",
+        5: "\n*\t\t*\n\n\t*\n\n*\t\t*\n",
+        6: "\n*\t\t*\n\n*\t\t*\n\n*\t\t*\n"
         }
-    print(f"\nEl dado número {i} ha generado aleatoriamente un:\n", posibilidades[resuldado])
+    print(f"\nEl dado número {i + 1} ha generado aleatoriamente un:\n", posibilidades[resuldado])
     return resuldado
 
 
 def entrada():
     try:
-        num = int(input("¿Cuántos dados lanzamos?"))
+        num = int(input("¿Cuántos dados lanzamos? "))
         return num
     except ValueError:
         print("Eso no es un número...")
@@ -25,7 +25,17 @@ def entrada():
 
 
 def salir(lista):
-    pass
+    set_lista = set(lista)
+    lista_ordenada = sorted(lista)
+    lista_porc = []
+    for i in range(1, 7):
+        lista_porc.append(lista_ordenada.count(i))
+    for i, elem in enumerate(lista_porc):
+        lista_porc[i] = (elem*100)/len(lista_ordenada)
+    print("Los valores de los dados lanzados fueron: ", ", ".join(str(x) for x in list(set_lista)))
+    print("Los porcentajes de salida son: ", "% ".join(str(x) for x in lista_porc), end="%")
+    return
+
 
 
 def menu():
